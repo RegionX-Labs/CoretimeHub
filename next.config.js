c8a6134b3bdcbdec86c322dcbb568c7622ab50d3
cuteolaf <oliverlim818@gmail.com>
2023-12-18 18:30:56 -0800

    set up env vars

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['github.com'],
   },
+  env: {
+    WS_CORETIME_CHAIN: process.env.WS_CORETIME_CHAIN,
+    WS_RELAY_CHAIN: process.env.WS_RELAY_CHAIN,
+    WS_CONTRACTS_CHAIN: process.env.WS_CONTRACTS_CHAIN,
+  },
 };
 
 module.exports = nextConfig;
