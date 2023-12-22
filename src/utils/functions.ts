79d735e87127c61b15293fc149a3e6952b64975e
cuteolaf <oliverlim818@gmail.com>
2023-12-21 22:54:38 -0800

    remove decodeMaskString

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,20 +1,6 @@
 import { ApiPromise } from '@polkadot/api';
 
-import { CoreMask, CoreMaskString, Timestamp } from '@/models';
-
-// decode core mask hex string into a Uint8Array
-export const decodeMaskString = (
-  strMask: CoreMaskString,
-  len: number
-): Uint8Array => {
-  const mask = strMask.slice(2);
-  const res = [];
-  for (let i = 0; i < len; ++i) {
-    res.push(parseInt(mask.slice(i * 2, i * 2 + 2), 16));
-  }
-  res.reverse();
-  return new Uint8Array(res);
-};
+import { CoreMask, Timestamp } from '@/models';
 
 // parse human readable number string
 export const parseHNString = (str: string): number => {
