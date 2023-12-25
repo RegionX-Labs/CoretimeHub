5e7711ca5ba1c913ec216594bccce924ca5ceac8
cuteolaf <oliverlim818@gmail.com>
2023-12-25 07:30:57 -0800

    update fetch block timestamp

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,11 @@
 import { ApiPromise } from '@polkadot/api';
 
-import { CoreMask, OnChainRegionId, Timestamp } from '@/models';
+import {
+  CoreMask,
+  OnChainRegionId,
+  RELAY_CHAIN_BLOCK_TIME,
+  Timestamp,
+} from '@/models';
 
 // parse human readable number string
 export const parseHNString = (str: string): number => {
@@ -11,22 +16,20 @@ export const getBlockTimestamp = async (
   api: ApiPromise,
   height: number
 ): Promise<Timestamp> => {
-  const hash = await api.rpc.chain.getBlockHash(height);
-  const apiAt = await api.at(hash);
-  const timestamp = Number((await apiAt.query.timestamp.now()).toJSON());
-  return timestamp;
-};
-
-export const countOne = (mask: CoreMask): number => {
-  let count = 0;
-  for (let i = 2; i < mask.length; ++i) {
-    let v = parseInt(mask.slice(i, i + 1), 16);
-    while (v > 0) {
-      if (v & 1) ++count;
-      v >>= 1;
-    }
+  const [resHeight, resTimestamp] = await Promise.all([
+    api.query.system.number(),
+    api.query.timestamp.now(),
+  ]);
+  const currentHeight = parseHNString(resHeight.toString());
+  const currentTimestamp = parseHNString(resTimestamp.toString());
+  if (height <= currentHeight) {
+    const hash = await api.rpc.chain.getBlockHash(height);
+    const apiAt = await api.at(hash);
+    const timestamp = Number((await apiAt.query.timestamp.now()).toJSON());
+    return timestamp;
+  } else {
+    return currentTimestamp + (height - currentHeight) * RELAY_CHAIN_BLOCK_TIME;
   }
-  return count;
 };
 
 export const stringifyOnChainId = (regionId: OnChainRegionId): string => {
@@ -41,4 +44,16 @@ export const parseOnChainId = (str: string): OnChainRegionId => {
     core: Number(strs[1]),
     mask: strs[2],
   } as OnChainRegionId;
+};
+
+export const countOne = (mask: CoreMask): number => {
+  let count = 0;
+  for (let i = 2; i < mask.length; ++i) {
+    let v = parseInt(mask.slice(i, i + 1), 16);
+    while (v > 0) {
+      if (v & 1) ++count;
+      v >>= 1;
+    }
+  }
+  return count;
 };
