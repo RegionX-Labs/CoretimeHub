2823fcf6721e27c50f2fb44f80c1181c951125de
Szegoo <sakacszergej@gmail.com>
2024-01-25 12:04:00 +0100

    Small improvements

--- src/pages/regions.tsx
+++ src/pages/regions.tsx
@@ -25,6 +25,7 @@ import {
   TransferIcon,
 } from '@/icons';
 import { RegionLocation } from '@/models';
+import { useToast } from '@/contexts/toast';
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -36,30 +37,42 @@ const Dashboard = () => {
   const [assignModalOpen, openAssignModal] = useState(false);
   const [transferModalOpen, openTransferModal] = useState(false);
 
+  const { toastInfo } = useToast();
+
   const selectedRegion =
     currentRegionIndex === undefined ? undefined : regions[currentRegionIndex];
   const regionSelected = selectedRegion !== undefined;
+
+  const manage = (openModal: (_v: boolean) => void) => {
+    if (!regionSelected) {
+      toastInfo(
+        'First select a region by clicking on one of the regions displayed.'
+      );
+    } else {
+      openModal(true);
+    }
+  };
 
   const management = [
     {
       label: 'partition',
       icon: PartitionIcon,
-      onClick: () => openPartitionModal(true),
+      onClick: () => manage(openPartitionModal),
     },
     {
       label: 'interlace',
       icon: InterlaceIcon,
-      onClick: () => openInterlaceModal(true),
+      onClick: () => manage(openInterlaceModal),
     },
     {
       label: 'transfer',
       icon: TransferIcon,
-      onClick: () => openTransferModal(true),
+      onClick: () => manage(openTransferModal),
     },
     {
       label: 'assign',
       icon: AssignmentIcon,
-      onClick: () => openAssignModal(true),
+      onClick: () => manage(openAssignModal),
     },
   ];
 
