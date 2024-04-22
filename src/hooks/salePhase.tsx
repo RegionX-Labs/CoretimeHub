8329716731dbf6fcfbbae8f497d14412e6622fe3
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2024-04-22 19:28:59 +0200

    Purchase components refactor (#78)
    * Purchase components refactor
    
    * lint fix

---
+++ src/hooks/salePhase.tsx
@@ -0,0 +1,104 @@
+import { ApiPromise } from '@polkadot/api';
+import { useCallback, useEffect, useState } from 'react';
+
+import {
+  getBlockTimestamp,
+  parseHNString,
+} from '@/utils/functions';
+import {
+  getCurrentPhase,
+  getSaleEndInBlocks,
+  getSaleProgress,
+  getSaleStartInBlocks,
+} from '@/utils/sale/utils';
+
+import { Section } from '@/components/Elements';
+
+import { useCoretimeApi } from '@/contexts/apis';
+import { ApiState } from '@/contexts/apis/types';
+import { useSaleInfo } from '@/contexts/sales';
+import { SalePhase } from '@/models';
+
+// Custom hook for fetching current phase
+const useSalePhase = () => {
+  const {
+    state: { api, apiState },
+  } = useCoretimeApi();
+  const { saleInfo, config } = useSaleInfo();
+
+  const [currentPhase, setCurrentPhase] = useState<SalePhase | null>(null);
+
+  const [saleEndTimestamp, setSaleEndTimestamp] = useState(0);
+  const [saleStartTimestamp, setSaleStartTimestamp] = useState(0);
+
+  const [progress, setProgress] = useState<number | null>(0);
+  const [saleSections, setSaleSections] = useState<Section[]>([]);
+
+  const fetchCurrentPhase = useCallback(
+    async (api: ApiPromise) => {
+      const blockNumber = (await api.query.system.number()).toJSON() as number;
+      const lastCommittedTimeslice = parseHNString(
+        (
+          (await api.query.broker.status()).toHuman() as any
+        ).lastCommittedTimeslice.toString()
+      );
+
+      const _saleStart = getSaleStartInBlocks(saleInfo, config);
+      const _saleEnd = getSaleEndInBlocks(
+        saleInfo,
+        blockNumber,
+        lastCommittedTimeslice,
+      );
+
+      getBlockTimestamp(api, _saleStart).then(
+        (value: number) => setSaleStartTimestamp(value)
+      );
+      getBlockTimestamp(api, _saleEnd).then(
+        (value: number) => setSaleEndTimestamp(value)
+      );
+
+      const progress = getSaleProgress(
+        saleInfo,
+        config,
+        blockNumber,
+        lastCommittedTimeslice,
+      );
+      setProgress(progress);
+
+      setCurrentPhase(getCurrentPhase(saleInfo, blockNumber));
+
+      const saleDuration = _saleEnd - _saleStart;
+
+      setSaleSections([
+        { name: 'Interlude', value: 0 },
+        {
+          name: 'Leadin phase',
+          value: (config.interludeLength / saleDuration) * 100,
+        },
+        {
+          name: 'Fixed price phase',
+          value:
+            ((config.interludeLength + config.leadinLength) / saleDuration) *
+            100,
+        },
+      ]);
+    },
+    [saleInfo, config]
+  );
+
+  useEffect(() => {
+    if (!api || apiState !== ApiState.READY) return;
+
+    fetchCurrentPhase(api);
+  }, [fetchCurrentPhase, api, apiState]);
+
+  return {
+    currentPhase,
+    saleStartTimestamp,
+    saleEndTimestamp,
+    progress,
+    saleSections,
+  };
+};
+
+export default useSalePhase;
---
+++ src/hooks/salePrice.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { getCurrentPrice } from '@/utils/sale/utils';
+
+import { useCoretimeApi } from '@/contexts/apis';
+import { ApiState } from '@/contexts/apis/types';
+import { useSaleInfo } from '@/contexts/sales';
+
+const useSalePrice = () => {
+  const {
+    state: { api, apiState },
+  } = useCoretimeApi();
+  const { saleInfo } = useSaleInfo();
+
+  const [currentPrice, setCurrentPrice] = useState(0);
+
+  const fetchCurrentPrice = useCallback(async () => {
+    if (api && apiState === ApiState.READY) {
+      const blockNumber = (await api.query.system.number()).toJSON() as number;
+      const price = getCurrentPrice(saleInfo, blockNumber);
+      setCurrentPrice(price);
+    }
+  }, [api, apiState, saleInfo]);
+
+  useEffect(() => {
+    fetchCurrentPrice();
+  }, [fetchCurrentPrice]);
+
+  return currentPrice;
+};
+
+export default useSalePrice;
