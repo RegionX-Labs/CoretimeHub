38269add588082eff35f2823ea2ce1c924a34e60
topeth <supernathanliu@gmail.com>
2024-05-14 06:24:14 -0700

    revert

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
     WS_REGIONX_CHAIN: process.env.WS_REGIONX_CHAIN || '',
     WS_ROCOCO_RELAY_CHAIN: process.env.WS_ROCOCO_RELAY_CHAIN,
     WS_KUSAMA_RELAY_CHAIN: process.env.WS_KUSAMA_RELAY_CHAIN,
+    EXPERIMENTAL: process.env.EXPERIMENTAL,
   },
 };
 
