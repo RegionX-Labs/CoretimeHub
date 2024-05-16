e4ac402d336f782c150536a3261ade82c29b3de3
Sergej <sakacszergej@gmail.com>
2024-05-16 15:35:42 +0200

    show correct data

--- src/contexts/regions/regionx/index.ts
+++ src/contexts/regions/regionx/index.ts
@@ -2,37 +2,45 @@ import { ApiPromise } from '@polkadot/api';
 import { Region } from 'coretime-utils';
 
 import { parseHNString } from '@/utils/functions';
+import { ISMPRecordStatus } from '@/models';
 
 export const fetchRegions = async (
   regionxApi: ApiPromise | null
-): Promise<Array<Region>> => {
+): Promise<Array<[Region, ISMPRecordStatus]>> => {
   if (!regionxApi) {
     return [];
   }
   try {
     const regionEntries = await regionxApi.query.regions.regions.entries();
 
-    const regions: Array<Region> = regionEntries
+    const regions: Array<[Region, ISMPRecordStatus]> = regionEntries
       .map(([key, value]) => {
         const keyTuple: any = key.toHuman(undefined, true);
         const { begin, core, mask } = keyTuple[0] as any;
-        const { owner, record: _record } = value.toHuman() as any;
+        const { owner, record } = value.toJSON() as any;
 
         const regionId = {
           begin: parseHNString(begin.toString()),
           core: parseHNString(core.toString()),
           mask: mask,
         };
-        return new Region(
-          regionId,
-          /*dummy data*/ {
-            end: 0,
-            owner,
-            paid: null,
-          }
-        );
+        const region = record.available
+          ? new Region(regionId, {
+              ...record.available,
+              owner,
+            })
+          : new Region(
+              regionId,
+              /*dummy data*/ {
+                end: 0,
+                owner,
+                paid: null,
+              }
+            );
+
+        return [region, ISMPRecordStatus.AVAILABLE];
       })
-      .filter((entry) => !!entry) as Array<Region>;
+      .filter((entry) => !!entry) as Array<[Region, ISMPRecordStatus]>;
     return regions;
   } catch (_) {
     return [];
