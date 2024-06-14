6e4668af64222b01a7d0b59a72f775bf879aa292
Sergej <sakacszergej@gmail.com>
2024-06-14 09:26:50 +0200

    fix account issue

--- src/contexts/account/index.tsx
+++ src/contexts/account/index.tsx
@@ -117,9 +117,8 @@ const AccountProvider = ({ children }: Props) => {
     if (accounts.length) {
       const activeAccount = localStorage.getItem(LOCAL_STORAGE_ACTIVE_ACCOUNT);
       const account: InjectedAccountWithMeta = activeAccount
-        ? accounts.find((acc: any) => acc.address == activeAccount) ??
-          accounts[0]
-        : null;
+        ? accounts.find((acc: any) => acc.address == activeAccount)
+        : accounts[0];
 
       setActiveAccount(account);
       localStorage.setItem(LOCAL_STORAGE_ACCOUNTS, JSON.stringify(accounts));
