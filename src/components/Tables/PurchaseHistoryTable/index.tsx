2d49c94691261dab64778543acf5e372fa608900
Nathanael Liu <supernathanliu@gmail.com>
2024-06-14 09:29:25 -0700

    feat: pagination controls for purchase history table (#164)
    * feat: pagination controls for purchase history table
    
    * read from environment
    
    * fix
    
    * rename
    
    ---------
    
    Co-authored-by: Sergej <sakacszergej@gmail.com>

--- src/components/Tables/PurchaseHistoryTable/index.tsx
+++ src/components/Tables/PurchaseHistoryTable/index.tsx
@@ -1,13 +1,17 @@
 import {
   Paper,
+  Stack,
   Table,
   TableBody,
   TableContainer,
+  TableFooter,
   TableHead,
+  TablePagination,
   TableRow,
 } from '@mui/material';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
+import { useState } from 'react';
 
 import { planckBnToUnit } from '@/utils/functions';
 
@@ -18,7 +22,7 @@ import { useCoretimeApi } from '@/contexts/apis';
 import { useNetwork } from '@/contexts/network';
 import { PurchaseHistoryItem } from '@/models';
 
-import { StyledTableCell } from '../common';
+import { StyledTableCell, StyledTableRow } from '../common';
 
 interface PurchaseHistoryTableProps {
   data: PurchaseHistoryItem[];
@@ -34,64 +38,103 @@ export const PurchaseHistoryTable = ({ data }: PurchaseHistoryTableProps) => {
     state: { symbol, decimals },
   } = useCoretimeApi();
 
+  // table pagination
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (
+    _event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer
-      component={Paper}
-      sx={{ maxHeight: '40rem', maxWidth: '100%' }}
-    >
-      <Table stickyHeader>
-        <TableHead>
+    <Stack direction='column' gap='1em'>
+      <TableContainer component={Paper} sx={{ height: '35rem' }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Extrinsic Idx</StyledTableCell>
+              <StyledTableCell>Account</StyledTableCell>
+              <StyledTableCell>Core</StyledTableCell>
+              <StyledTableCell>{`Price (${symbol})`}</StyledTableCell>
+              <StyledTableCell>Sales Type</StyledTableCell>
+              <StyledTableCell>Timestamp</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(
+              (
+                { address, core, extrinsic_index, timestamp, price, type },
+                index
+              ) => (
+                <StyledTableRow key={index}>
+                  <StyledTableCell>
+                    <Link
+                      href={`${SUBSCAN_URL[network]}/extrinsic/${extrinsic_index}`}
+                      target='_blank'
+                    >
+                      {extrinsic_index}
+                    </Link>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Link
+                      href={`${SUBSCAN_URL[network]}/account/${address}`}
+                      target='_blank'
+                    >
+                      <Address
+                        value={address}
+                        isCopy={true}
+                        isShort={true}
+                        size={24}
+                      />
+                    </Link>
+                  </StyledTableCell>
+                  <StyledTableCell align='right'>{core}</StyledTableCell>
+                  <StyledTableCell align='right'>
+                    {planckBnToUnit(price.toString(), decimals)}
+                  </StyledTableCell>
+                  <StyledTableCell>{type}</StyledTableCell>
+                  <StyledTableCell>
+                    {timeAgo.format(timestamp * 1000, 'round-minute')}
+                  </StyledTableCell>
+                </StyledTableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Stack alignItems='center'>
+        <TableFooter sx={{ alignItems: 'center' }}>
           <TableRow>
-            <StyledTableCell>Extrinsic Idx</StyledTableCell>
-            <StyledTableCell>Account</StyledTableCell>
-            <StyledTableCell>Core</StyledTableCell>
-            <StyledTableCell>{`Price (${symbol})`}</StyledTableCell>
-            <StyledTableCell>Sales Type</StyledTableCell>
-            <StyledTableCell>Timestamp</StyledTableCell>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    'aria-label': 'rows per page',
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(
-            (
-              { address, core, extrinsic_index, timestamp, price, type },
-              index
-            ) => (
-              <TableRow key={index}>
-                <StyledTableCell>
-                  <Link
-                    href={`${SUBSCAN_URL[network]}/extrinsic/${extrinsic_index}`}
-                    target='_blank'
-                  >
-                    {extrinsic_index}
-                  </Link>
-                </StyledTableCell>
-                {/** FIXME: replace with the Address component */}
-                <StyledTableCell>
-                  <Link
-                    href={`${SUBSCAN_URL[network]}/account/${address}`}
-                    target='_blank'
-                  >
-                    <Address
-                      value={address}
-                      isCopy={true}
-                      isShort={true}
-                      size={24}
-                    />
-                  </Link>
-                </StyledTableCell>
-                <StyledTableCell align='right'>{core}</StyledTableCell>
-                <StyledTableCell align='right'>
-                  {planckBnToUnit(price.toString(), decimals)}
-                </StyledTableCell>
-                <StyledTableCell>{type}</StyledTableCell>
-                <StyledTableCell>
-                  {timeAgo.format(timestamp * 1000, 'round-minute')}
-                </StyledTableCell>
-              </TableRow>
-            )
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        </TableFooter>
+      </Stack>
+    </Stack>
   );
 };
