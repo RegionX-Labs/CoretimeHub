4845a97fd81a62b24e17f16f6d5cdfba637d92dc
cuteolaf <oliverlim818@gmail.com>
2024-06-14 00:01:15 -0700

    fix error handling

--- src/utils/functions/api.ts
+++ src/utils/functions/api.ts
@@ -3,27 +3,32 @@ import { ISubmittableResult, Signer } from '@polkadot/types/types';
 
 import { TxStatusHandlers } from '@/models';
 
-export const sendTx = (
+export const sendTx = async (
   tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
   account: AddressOrPair,
   signer: Signer,
   handlers: TxStatusHandlers
 ) => {
   try {
-    tx.signAndSend(account, { signer }, ({ status, events }) => {
-      if (status.isReady) handlers.ready();
-      else if (status.isInBlock) handlers.inBlock();
-      else if (status.isFinalized) {
-        handlers.finalized();
-        events.forEach(({ event: { method } }) => {
-          if (method === 'ExtrinsicSuccess') {
-            handlers.success();
-          } else if (method === 'ExtrinsicFailed') {
-            handlers.fail();
-          }
-        });
+    const unsub = await tx.signAndSend(
+      account,
+      { signer },
+      ({ status, events }) => {
+        if (status.isReady) handlers.ready();
+        else if (status.isInBlock) handlers.inBlock();
+        else if (status.isFinalized) {
+          handlers.finalized();
+          events.forEach(({ event: { method } }) => {
+            if (method === 'ExtrinsicSuccess') {
+              handlers.success();
+            } else if (method === 'ExtrinsicFailed') {
+              handlers.fail();
+            }
+          });
+          unsub();
+        }
       }
-    });
+    );
   } catch (e) {
     handlers.error(e);
   } finally {
